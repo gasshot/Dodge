2aef33069074e4831d232a316f53d388ba6b90f3
gasshot <kanginoh@gmail.com>
2023-04-14 13:50:01 +0900

    202304141349
    ㅋㅋ

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 bulletRigidbody에 할당
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        // 리지드바디의 속도 = 앞쪽 방향  * 이동속력
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
